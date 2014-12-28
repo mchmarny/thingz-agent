fc26d20e709d80b9bb0cdf908a43d2f4f51817f6
mchmarny <mark@chmarny.com>
2014-12-27 19:08:50 -0800

    Remote server support

---
+++ scripts/run-remote.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$(pwd)"
+
+$DIR/thingz-agent --source="${HOSTNAME}" \
+                  --strategy=cpu:10,cpus:60,mem:15,swap:30,load:15 \
+                  --publisher="http://agent:${THINGZ_SECRET}@${THINGZ_HOST}:8086/thingz" \
+                  --verbose=true
